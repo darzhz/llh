5cfbcacd44c93ef5db77f6024a8c125c460491fb
Darsh shyam kumar <68496521+darzhz@users.noreply.github.com>
2021-01-16 16:15:44 +0530

    sa

--- serv.js
+++ serv.js
@@ -3,7 +3,7 @@ const { Telegraf } = require('telegraf')
 const app =  new Telegraf("1339644712:AAE7Wb3aAwNPHKkWgveC_H2f8FR_DmRpRd0")
 let serv = express();
 
-serv.get("/",(req,res) => {
+serv.get("/serv",(req,res) => {
 	app.telegram.sendMessage(893924820,"someone has visited your site")
 })
 //app.telegram.sendMessage(893924820,"whatsup my man")
